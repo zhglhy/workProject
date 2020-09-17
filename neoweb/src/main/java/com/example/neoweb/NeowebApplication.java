0fc8396b0c46b85dd69faf4731dfe195740237c5
zhglhy <zhglhy@126.com>
2020-09-17 18:34:16 +0800

    Add files via upload
    neo4j test

---
+++ neoweb/src/main/java/com/example/neoweb/NeowebApplication.java
@@ -0,0 +1,13 @@
+package com.example.neoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeowebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeowebApplication.class, args);
+    }
+
+}
